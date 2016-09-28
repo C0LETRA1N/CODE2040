ab9aba3ca253cd1a30b213f8cd4e996473489418
Christopher Cole <christophercole1987@icloud.com>
2016-09-28 14:51:48 -0500

    Finished step 2

---
+++ reverse.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+url = 'http://challenge.code2040.org/api/reverse'
+dictionary = {"token": "f9ac431a86df26c7ad79ddaf8a1cf05f"}
+r = requests.post(url, json=dictionary)
+print(r.text)
+print(r.text[::-1])
+
+url = 'http://challenge.code2040.org/api/reverse/validate'
+dictionary = {"token": "f9ac431a86df26c7ad79ddaf8a1cf05f", "string": r.text[::-1]}
+r = requests.post(url, json=dictionary)
+print(r)
