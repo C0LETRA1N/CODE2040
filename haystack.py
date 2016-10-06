f6c105b5572fb970fbf1773a4f78c84186b89cd9
Christopher Cole <christophercole1987@icloud.com>
2016-10-06 13:13:56 -0500

    Finished step 4

--- haystack.py
+++ haystack.py
@@ -9,11 +9,11 @@ haystack = r.json().get('haystack')
 print(needle)
 pos = 0
 while pos < len(haystack):
-    if (needle == haystack[pos]):
+    if needle == haystack[pos]:
         print(pos)
         break
     pos += 1
 url = 'http://challenge.code2040.org/api/haystack/validate'
 dictionary = {"token": "f9ac431a86df26c7ad79ddaf8a1cf05f", "needle": pos}
-r = requests.post(url, json=dictionary)
+r = requests.post(url, json = dictionary)
 print(r)
---
+++ prefix.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+
+url = 'http://challenge.code2040.org/api/prefix'
+dictionary = {"token": "f9ac431a86df26c7ad79ddaf8a1cf05f"}
+r = requests.post(url, json=dictionary)
+
+prefix = r.json().get('prefix')
+array = r.json().get('array')
+
+pos = 0
+found = []
+while pos < len(array):
+    if not array[pos].startswith(prefix):
+        found.append(array[pos])
+    pos += 1
+
+url = 'http://challenge.code2040.org/api/prefix/validate'
+dictionary = {'token': "f9ac431a86df26c7ad79ddaf8a1cf05f", 'array': found}
+r = requests.post(url, json=dictionary)
+if r.status_code != 200:
+    print('Status: ', r.status_code, 'Error: ', r.reason)
+else:
+    print r
